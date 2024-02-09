abce4074c256f0a6a583be77ea14bc34120d9575
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-09 17:07:50 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
-  <section class="min-h-screen">
+  <section>
     <h2 class="text-yellow text-center">
       เลือกหมวดหมู่ที่ต้องการ
     </h2>
     <br />
-    
   </section>
 </template>
