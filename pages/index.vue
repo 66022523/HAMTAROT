a7cc5c041b214cbd9accafe9e8269f503c4bc19d
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-09 17:01:05 +0700

    🖌️ Use color of theme set

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,9 @@
 <template>
   <section class="min-h-screen">
-    <h2 class="ham-yellow text-center">
+    <h2 class="text-yellow text-center">
       เลือกหมวดหมู่ที่ต้องการ
     </h2>
     <br />
     
   </section>
-  
 </template>
