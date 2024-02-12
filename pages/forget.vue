c0179896ee77df3a8d2be786630f0d55030487ac
Kanyawee <66025997@gmail.com>
2024-02-12 22:43:12 +0700

    Login

---
+++ pages/forget.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h2 class="text-yellow text-center">
+      เลือกหมวดหมู่ที่ต้องการ
+    </h2>
+    <br />
+    <div>
+      <ForgetPasswordFrom />
+    </div>
+  </section>
+</template>
