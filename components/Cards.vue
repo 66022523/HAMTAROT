fdb905b0b918743ff853145850398ea335ad7c79
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-13 01:49:44 +0700

    🎴 Redesign card component

--- components/Cards.vue
+++ components/Cards.vue
@@ -1,48 +1,32 @@
 <template>
-  <div class="card">
-    <div class="background">
-      <div class="frame">
-        <div class="circle-icon">
-        </div>
-      </div>
+  <div class="card flex items-center justify-center">
+    <div class="card-icon">
+      <img :src="icon" alt="" width="80px" />
     </div>
   </div>
 </template>
 
 <style scoped>
 .card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 225px;
+  height: 355px;
+  border-radius: 20px;
+  border: 15px solid white;
+  background-color: #3b3486;
 }
 
-.background {
-  background-color: #F8EDFF;
-  width: 150px;
-  height: 220px;
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.frame {
-  background-color: #3B3486;
-  width: 133px;
-  height: 205px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.circle-icon {
-  background-color: #F8EDFF;
-  width: 60px;
-  height: 60px;
-  border-radius: 50%;
-  background-image: url('/ICON/Chat.png'); 
-  background-size: cover; 
-  border: 5px solid white;
+.card-icon {
+  border-radius: 100%;
+  background-color: #f8edff;
+  border: 8px solid white;
 }
 </style>
+
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    default: "/favicon.ico",
+  },
+});
+</script>
