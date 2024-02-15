d676b461f0f68089e2c41419b1e710159d5a847b
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-16 03:22:24 +0700

    ✔️ Fix warning error and set default text color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="text-primary-content">
+  <div>
     <header class="sticky top-0 left-0 right-0 z-50 container mx-auto py-10">
       <Navbar />
     </header>
     <main class="relative">
       <slot />
-      <Contact />
     </main>
+    <footer>
+      <Contact />
+    </footer>
   </div>
 </template>
