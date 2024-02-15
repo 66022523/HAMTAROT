9a7e4bbfc21197d3099ebb799a5762aa64ed509b
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-15 15:53:57 +0700

    📏 Fix card position

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <section class="container mx-auto text-pink text-center">
     <h2 class="text-yellow mb-8">เลือกหมวดหมู่ที่ต้องการ</h2>
-    <div
-      class="grid grid-cols-3 gap-8 justify-center items-center content-center"
-    >
+    <div class="flex flex-wrap gap-8 justify-center items-center">
       <NuxtLink
         v-for="index in categories.length"
         :key="index"
         :to="{ name: 'pick', query: { category: categories[index - 1] } }"
-        class="justify-self-center transition hover:scale-105 hover:underline"
+        class="basis-1/4 flex flex-col justify-center items-center transition hover:scale-105 hover:underline"
       >
         <Cards
           class="mb-4 transition hover:bg-purple"
