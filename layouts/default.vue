254040775bbb80a03009e738e1cc2b4ee625f882
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-15 15:49:56 +0700

    ✅ Fix code warning

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
-  <Header />
-  <main class="text-primary-content py-36">
-    <slot />
-    <Contact />
-  </main>
+  <div class="text-primary-content">
+    <header class="sticky top-0 left-0 right-0 z-50 container mx-auto py-10">
+      <Navbar />
+    </header>
+    <main class="relative">
+      <slot />
+      <Contact />
+    </main>
+  </div>
 </template>
