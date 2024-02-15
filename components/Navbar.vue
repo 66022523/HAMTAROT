e563787e839e3ff03ec0189e25289b8d8ae558d1
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-15 15:48:51 +0700

    ✂️ Spread navbar from header

---
+++ components/Navbar.vue
@@ -0,0 +1,86 @@
+<template>
+  <nav
+    class="navbar p-3 border border-indigo-950 bg-[#3B348620] rounded-xl shadow-xl backdrop-blur"
+  >
+    <div class="navbar-start">
+      <a class="btn btn-ghost btn-circle" href="/">
+        <img
+          src="/favicon.ico"
+          alt="Hamtarot Favicon"
+          width="50px"
+          height="50px"
+        />
+      </a>
+    </div>
+    <div class="navbar-center">
+      <a
+        class="btn btn-ghost text-3xl text-pink"
+        style="
+          font-family: &quot;Cherry Bomb One&quot;, system-ui;
+          font-weight: normal;
+        "
+        href="/"
+      >
+        HAMTAROT
+      </a>
+    </div>
+    <div class="navbar-end">
+      <!-- ? If the user has already logged in -->
+      <!-- <div class="dropdown dropdown-end">
+          <div
+            tabindex="0"
+            role="button"
+            class="btn btn-ghost btn-circle avatar"
+          >
+            <div class="w-10 rounded-full">
+              <img
+                alt="Profile"
+                src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+              />
+            </div>
+          </div>
+          <ul
+            tabindex="0"
+            class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li class="menu-title">จัดการบัญชี</li>
+            <li>
+              <a href="/account/edit">แก้ไขโปรไฟล์</a>
+            </li>
+            <li><a>ลงชื่อออก</a></li>
+          </ul>
+        </div> -->
+
+      <!-- ? If the user has not logged in yet -->
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-circle avatar">
+          <div class="w-10 rounded-full">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-10 h-10"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </div>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li class="menu-title">จัดการบัญชี</li>
+          <li><a href="/account/login">ลงชื่อเข้าใช้</a></li>
+          <li><a href="/account/register">สมัครบัญชีใหม่</a></li>
+          <li><a href="/account/forget">ลืมรหัสผ่าน</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
