2f009a5f9505730283e2015858160efe5d727c5d
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-16 03:17:10 +0700

    🌟 Change default checked to five stars

--- components/Rating.vue
+++ components/Rating.vue
@@ -19,27 +19,27 @@
           type="radio"
           name="rating"
           class="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating"
+          class="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating"
+          class="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating"
+          class="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating"
+          class="mask mask-star-2 bg-orange-400"
           checked
-        />
-        <input
-          type="radio"
-          name="rating"
-          class="mask mask-star-2 bg-orange-400"
-        />
-        <input
-          type="radio"
-          name="rating"
-          class="mask mask-star-2 bg-orange-400"
-        />
-        <input
-          type="radio"
-          name="rating"
-          class="mask mask-star-2 bg-orange-400"
-        />
-        <input
-          type="radio"
-          name="rating"
-          class="mask mask-star-2 bg-orange-400"
         />
       </div>
       <div class="modal-action flex justify-center">
