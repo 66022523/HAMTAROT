d9e8c2e37641a6842f064c201ac159d64313dde6
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-16 03:15:07 +0700

    🕳️ Add shadow color

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    class="btn btn-circle btn-warning fixed bottom-5 right-5"
+    class="btn btn-circle btn-warning shadow-md shadow-yellow fixed bottom-5 right-5"
     href="mailto:66022523@up.ac.th"
   >
     <i class="fi fi-br-question"></i>
