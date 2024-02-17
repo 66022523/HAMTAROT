4c4d460e243df75ca608ae73b13f5aa9dc9bf112
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-17 11:50:20 +0700

    ⛰️ Update Nuxt configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/image", "@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
-  googleFonts: {
-    families: {
-      Kanit: {
-        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-        ital: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      },
-      "Cherry Bomb One": true,
-    },
-  },
   app: {
-    pageTransition: { name: "page", mode: "out-in" },
-    layoutTransition: { name: "page", mode: "out-in" },
     head: {
       title: "Hamtarot",
       meta: [
@@ -31,6 +17,25 @@ export default defineNuxtConfig({
       script: [],
       noscript: [],
     },
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "page", mode: "out-in" },
+    rootId: "__hamtarot",
+    rootTag: "body",
   },
+  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+  experimental: {
+    viewTransition: true,
+  },
+  modules: ["@nuxt/image", "@nuxtjs/google-fonts", "@nuxtjs/tailwindcss"],
   ssr: false,
+  googleFonts: {
+    families: {
+      Kanit: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        ital: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+      "Cherry Bomb One": true,
+    },
+  },
 });
