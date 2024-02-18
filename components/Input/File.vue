c5531394911dd76a6efefc40ad10da1b72327088
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-18 18:25:47 +0700

    🔣 Add new inputs

---
+++ components/Input/File.vue
@@ -0,0 +1,60 @@
+<script setup>
+defineProps({
+  inputClass: {
+    type: String,
+    default: "",
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  onChange: {
+    type: Function,
+    default: null,
+  },
+  hintTopLeft: {
+    type: String,
+    default: "",
+  },
+  hintTopRight: {
+    type: String,
+    default: "",
+  },
+  hintBottomLeft: {
+    type: String,
+    default: "",
+  },
+  hintBottomRight: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <label class="form-control">
+    <div v-if="hintTopLeft || hintTopRight" class="label">
+      <span v-if="hintTopLeft" class="label-text">
+        {{ hintTopLeft }}
+      </span>
+      <span v-if="hintTopRight" class="label-text-alt">
+        {{ hintTopRight }}
+      </span>
+    </div>
+    <input
+      type="file"
+      class="file-input file-input-bordered w-full"
+      :class="inputClass"
+      :required="required"
+      @change="onChange"
+    />
+    <div v-if="hintBottomLeft || hintBottomRight" class="label">
+      <span v-if="hintBottomLeft" class="label-text-alt">
+        {{ hintBottomLeft }}
+      </span>
+      <span v-if="hintBottomRight" class="label-text-alt">
+        {{ hintBottomRight }}
+      </span>
+    </div>
+  </label>
+</template>
---
+++ components/Input/Textarea.vue
@@ -0,0 +1,72 @@
+<script setup>
+const model = defineModel({ type: String });
+
+defineProps({
+  inputClass: {
+    type: String,
+    default: null,
+  },
+  placeholder: {
+    type: String,
+    default: null,
+  },
+  minLength: {
+    type: String,
+    default: null,
+  },
+  maxLength: {
+    type: String,
+    default: null,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  hintTopLeft: {
+    type: String,
+    default: null,
+  },
+  hintTopRight: {
+    type: String,
+    default: null,
+  },
+  hintBottomLeft: {
+    type: String,
+    default: null,
+  },
+  hintBottomRight: {
+    type: String,
+    default: null,
+  },
+});
+</script>
+
+<template>
+  <label class="form-control">
+    <div v-if="hintTopLeft || hintTopRight" class="label">
+      <span v-if="hintTopLeft" class="label-text">
+        {{ hintTopLeft }}
+      </span>
+      <span v-if="hintTopRight" class="label-text-alt">
+        {{ hintTopRight }}
+      </span>
+    </div>
+    <textarea
+      v-model="model"
+      class="textarea textarea-bordered"
+      :class="inputClass"
+      :placeholder="placeholder"
+      :minlength="minLength"
+      :maxlength="maxLength"
+      :required="required"
+    />
+    <div v-if="hintBottomLeft || hintBottomRight" class="label">
+      <span v-if="hintBottomLeft" class="label-text-alt">
+        {{ hintBottomLeft }}
+      </span>
+      <span v-if="hintBottomRight" class="label-text-alt">
+        {{ hintBottomRight }}
+      </span>
+    </div>
+  </label>
+</template>
