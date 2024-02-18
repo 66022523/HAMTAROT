310070d8838546715d72474e2d5a11b37f2c67a6
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-19 02:08:29 +0700

    🎩 Move setup script to top

--- components/Card/Result.vue
+++ components/Card/Result.vue
@@ -1,24 +1,3 @@
-<template>
-  <div
-    class="card mb-8 overflow-hidden rounded-badge bg-blackcurrant shadow-xl before:absolute before:left-0 before:top-0 before:block before:size-full before:bg-[url('/favicon.ico')] before:bg-[length:100px_100px] before:bg-[50px_50px] before:bg-repeat before:opacity-5 before:content-['']"
-  >
-    <div class="card-body relative">
-      <CardTarot
-        class="mx-auto mb-4"
-        flip
-        :number="number"
-        :image="image"
-        :title="title"
-      />
-      <h3>{{ name }}</h3>
-      <span>{{ info }}</span>
-      <p class="mt-2">
-        {{ content }}
-      </p>
-    </div>
-  </div>
-</template>
-
 <script setup>
 defineProps({
   number: {
@@ -47,3 +26,24 @@ defineProps({
   },
 });
 </script>
+
+<template>
+  <div
+    class="card mb-8 overflow-hidden rounded-badge bg-blackcurrant shadow-xl before:absolute before:left-0 before:top-0 before:block before:size-full before:bg-[url('/favicon.ico')] before:bg-[length:100px_100px] before:bg-[50px_50px] before:bg-repeat before:opacity-5 before:content-['']"
+  >
+    <div class="card-body relative">
+      <CardTarot
+        class="mx-auto mb-4"
+        flip
+        :number="number"
+        :image="image"
+        :title="title"
+      />
+      <h3>{{ name }}</h3>
+      <span>{{ info }}</span>
+      <p class="mt-2">
+        {{ content }}
+      </p>
+    </div>
+  </div>
+</template>
