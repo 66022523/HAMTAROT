096e2f8e675d495f5832cc69b85ba21a888fc208
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-18 18:26:19 +0700

    🧭 Spread navbar from admin to component

---
+++ components/Dashboard/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup>
+defineProps({
+  activePath: {
+    type: String,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div role="tablist" class="tabs-boxed tabs tabs-lg rounded-xl">
+    <NuxtLink
+      role="tab"
+      class="tab"
+      :class="{ 'tab-active': activePath === '/dashboard/statistics' }"
+      to="/dashboard/statistics"
+    >
+      สถิติ
+    </NuxtLink>
+    <NuxtLink
+      role="tab"
+      class="tab"
+      :class="{ 'tab-active': activePath === '/dashboard/notification' }"
+      to="/dashboard/notification"
+    >
+      การแจ้งเตือน
+    </NuxtLink>
+    <NuxtLink
+      role="tab"
+      class="tab"
+      :class="{ 'tab-active': activePath === '/dashboard/users' }"
+      to="/dashboard/users"
+    >
+      จัดการผู้ใช้งาน
+    </NuxtLink>
+  </div>
+</template>
