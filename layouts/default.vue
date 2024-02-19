60e571fcaa6baae263d2fe767e5510b702c97f96
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-19 17:01:54 +0700

    ➕ Add speed insights tool

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/nuxt";
+</script>
+
 <template>
   <div>
     <header class="container sticky left-0 right-0 top-0 z-50 mx-auto py-10">
@@ -9,5 +13,6 @@
     <footer>
       <ButtonContact />
     </footer>
+    <SpeedInsights />
   </div>
 </template>
