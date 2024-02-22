f72c2eea66de87339523d39c5bbc4fec22b60116
Chaiwat Suwannarat <66022523@up.ac.th>
2024-02-22 15:55:36 +0700

    🔨 Fix issue

--- components/Button/Contact.vue
+++ components/Button/Contact.vue
@@ -1,11 +1,9 @@
 <template>
-  <address>
-    <a
-      class="btn btn-circle btn-warning fixed bottom-5 right-5 shadow-md shadow-portica"
-      href="mailto:66022523@up.ac.th"
-      aria-label="ติดต่อพวกเราด้วยอีเมล"
-    >
-      <i class="fi fi-br-question"></i>
-    </a>
-  </address>
+  <a
+    class="btn btn-circle btn-warning shadow-lg"
+    href="mailto:66022523@up.ac.th"
+    aria-label="ติดต่อพวกเราด้วยอีเมล"
+  >
+    <i class="fi fi-br-question"></i>
+  </a>
 </template>
